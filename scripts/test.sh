eb1ec344c2e72fcbcd352d01ae86d9891b22a042
ichlubna <43234438+ichlubna@users.noreply.github.com>
2022-12-14 08:06:28 +0100

    Add files via upload

---
+++ scripts/test.sh
@@ -0,0 +1,21 @@
+A=()
+for i in {1..100}
+do
+	#B=$(ffmpeg -ss 00:00:00 -c:v hevc_cuvid -i 30.mkv -frames:v 5 -benchmark -f null - 2>&1)
+	#B=$(ffmpeg -ss 00:00:00 -c:v hevc_cuvid -i 830.mkv -frames:v 5 -benchmark -f null - 2>&1)
+	#B=$(ffmpeg -ss 00:00:06 -c:v hevc_cuvid -i allI.mkv -frames:v 2 -benchmark -f null - 2>&1)
+	#B=$(ffmpeg -ss 00:00:06.7 -c:v hevc_cuvid -i allI.mkv -frames:v 2 -benchmark -f null - 2>&1)
+	#B=$(ffmpeg -ss 00:00:06.7 -c:v hevc_cuvid -i allI.mkv -frames:v 4 -benchmark -f null - 2>&1)
+	#B=$(ffmpeg -ss 00:00:01.2 -c:v hevc_cuvid -i 8allI.mkv -frames:v 2 -benchmark -f null - 2>&1)
+	#B=$(ffmpeg -ss 00:00:01.5 -c:v hevc_cuvid -i 8allI.mkv -frames:v 2 -benchmark -f null - 2>&1)
+	#B=$(ffmpeg -ss 00:00:06 -c:v hevc_cuvid -i 30.mkv -frames:v 2 -benchmark -f null - 2>&1)
+	#B=$(ffmpeg -ss 00:00:06.7 -c:v hevc_cuvid -i 30.mkv -frames:v 2 -benchmark -f null - 2>&1)
+	#B=$(ffmpeg -ss 00:00:01.2 -c:v hevc_cuvid -i 830.mkv -frames:v 2 -benchmark -f null - 2>&1)
+	B=$(ffmpeg -ss 00:00:01.5 -c:v hevc_cuvid -i 830.mkv -frames:v 2 -benchmark -f null - 2>&1)
+	C=$(grep -oP '(?<=utime=).*?(?=s)' <<< "$B")
+	A[${#A[@]}]=$C
+done
+for i in {1..100}
+do
+echo ${A[$i]}
+done
